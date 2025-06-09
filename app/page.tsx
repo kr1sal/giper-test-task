ab75a9040f27e8e204dd04b04d9e37509fac4b67
kr1sal <kirillivanov523@gmail.com>
2025-06-09 20:15:24 +0300

    feat: stylize the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <nav className="flex flex-col">
-        <ul className="">
-          <li className="">
-            <Link href="/todo">To Do</Link>
-          </li>
-          <li className="">
-            <Link href="/posts">Posts</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="flex flex-col items-center justify-center min-h-screen">
+      <ul className="menu bg-base-200 rounded-box">
+        <li>
+          <Link href="/todo" className="text-center ml-auto mr-auto text-2xl">To Do</Link>
+        </li>
+        <li>
+          <Link href="/posts" className="text-center ml-auto mr-auto text-2xl">Posts</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
